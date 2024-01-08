45e31ec0384a35b8ec046dccefd656134a5f9d55
Vishal Chaurasiya <129888819+Vishal35198@users.noreply.github.com>
2024-01-08 23:30:19 +0530

    image showing at home screen

--- lib/Screens/name.dart
+++ lib/Screens/name.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:dsc_project/Screens/home.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +9,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class NameScreen extends StatefulWidget {
-  const NameScreen({super.key});
+  const NameScreen({Key? key});
 
   @override
   State<NameScreen> createState() => _NameScreenState();
@@ -17,7 +18,9 @@ class NameScreen extends StatefulWidget {
 class _NameScreenState extends State<NameScreen> {
   TextEditingController _controller = TextEditingController();
   String name = '';
-  File? _selectedImage = null;
+  File? _selectedImage;
+  Uint8List? _imageBytes;
+
   Future<void> setName(String name) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString('name', name);
@@ -25,20 +28,17 @@ class _NameScreenState extends State<NameScreen> {
     print("done");
   }
 
-  Future<void> setImage(String path) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString('imagePath', path);
-
-    print("done");
-  }
-
-  Future<void> pickAndDisplayImage() async {
+  void pickAndDisplayImage() async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
+      List<int> bytes = await pickedFile.readAsBytes();
+      Uint8List imageBytes = Uint8List.fromList(bytes);
+
       setState(() {
         _selectedImage = File(pickedFile.path);
+        _imageBytes = imageBytes;
       });
     } else {
       print("No image selected");
@@ -85,8 +85,12 @@ class _NameScreenState extends State<NameScreen> {
     final pickedFile = await picker.pickImage(source: ImageSource.camera);
 
     if (pickedFile != null) {
+      List<int> bytes = await pickedFile.readAsBytes();
+      Uint8List imageBytes = Uint8List.fromList(bytes);
+
       setState(() {
         _selectedImage = File(pickedFile.path);
+        _imageBytes = imageBytes;
       });
     } else {
       print("No image selected");
@@ -102,114 +106,125 @@ class _NameScreenState extends State<NameScreen> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Expanded(
-                flex: 1,
-                child: Text(
-                  "Doingly",
-                  style: GoogleFonts.spaceGrotesk(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    "Doingly",
+                    style: GoogleFonts.spaceGrotesk(
                       fontWeight: FontWeight.bold,
                       fontSize: 35,
-                      color: Color(0xffFF4655)),
+                      color: Color(0xffFF4655),
+                    ),
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 1,
-                child: _selectedImage != null
-                    ? GestureDetector(
-                        onTap: () {
-                          _pickImage();
-                        },
-                        child: CircleAvatar(
-                          radius: 100,
-                          backgroundImage: FileImage(_selectedImage!),
-                        ),
-                      )
-                    : GestureDetector(
-                        onTap: () {
-                          _pickImage();
-                        },
-                        child: Stack(
-                          alignment: Alignment.bottomRight,
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: Colors.red[100],
-                              radius: 100,
-                              child: Image.asset(
-                                "assets/images/name2.png",
+                Expanded(
+                  flex: 1,
+                  child: _selectedImage != null
+                      ? GestureDetector(
+                          onTap: () {
+                            _pickImage();
+                          },
+                          child: CircleAvatar(
+                            radius: 100,
+                            backgroundImage: MemoryImage(_imageBytes!),
+                          ),
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            _pickImage();
+                          },
+                          child: Stack(
+                            alignment: Alignment.bottomRight,
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Colors.red[100],
+                                radius: 100,
+                                child: Image.asset(
+                                  "assets/images/name2.png",
+                                ),
                               ),
-                            ),
-                            const CircleAvatar(
-                              backgroundColor: Color(0xFFFA8484),
-                              radius: 28,
-                              child: Icon(Icons.add),
-                            )
-                          ],
+                              const CircleAvatar(
+                                backgroundColor: Color(0xFFFA8484),
+                                radius: 28,
+                                child: Icon(Icons.add),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Expanded(
-                flex: 2,
-                child: TextFormField(
-                  onChanged: (value) {
-                    name = value;
-                  },
-                  controller: _controller,
-                  style: GoogleFonts.spaceGrotesk(
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: TextFormField(
+                    onChanged: (value) {
+                      name = value;
+                    },
+                    controller: _controller,
+                    style: GoogleFonts.spaceGrotesk(
                       fontSize: 14,
                       color: Colors.black,
-                      fontWeight: FontWeight.bold),
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
+                      fontWeight: FontWeight.bold,
+                    ),
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(16),
                         borderSide: BorderSide(
                           color: Color(0xffFF4655),
-                        )),
-                    enabledBorder: OutlineInputBorder(
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(16),
                         borderSide: BorderSide(
                           color: Colors.grey,
-                        )),
-                    contentPadding: EdgeInsets.all(15),
-                    labelStyle: GoogleFonts.spaceGrotesk(
+                        ),
+                      ),
+                      contentPadding: EdgeInsets.all(15),
+                      labelStyle: GoogleFonts.spaceGrotesk(
                         fontSize: 14,
                         color: Colors.grey[600],
-                        fontWeight: FontWeight.bold),
-                    labelText: "Write your name.",
-                    fillColor: Color(0xff4655),
+                        fontWeight: FontWeight.bold,
+                      ),
+                      labelText: "Write your name.",
+                      fillColor: Color(0xff4655),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                width: 300,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xffFF4655)),
-                  onPressed: () {
-                    if (name == "") {
-                      Get.snackbar("Going without typing name?",
-                          "Please tell us your respected name.");
-                    } else {
-                      setName(name);
-                      _selectedImage == null
-                          ? Get.defaultDialog(
-                              backgroundColor: Colors.white,
-                              titleStyle: GoogleFonts.spaceGrotesk(
-                                  fontSize: 24, color: Colors.red),
-                              title: "Alert",
-                              titlePadding: EdgeInsets.only(top: 10),
-                              content: Text(
-                                "Are you sure to go without an Image?",
-                                style: TextStyle(fontSize: 18),
-                              ),
-                              contentPadding:
-                                  EdgeInsets.symmetric(horizontal: 12),
-                              confirm: TextButton(
+                Container(
+                  width: 300,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xffFF4655),
+                    ),
+                    onPressed: () {
+                      if (name.isEmpty) {
+                        Get.snackbar(
+                          "Going without typing name?",
+                          "Please tell us your respected name.",
+                        );
+                      } else {
+                        setName(name);
+                        _selectedImage == null
+                            ? Get.defaultDialog(
+                                backgroundColor: Colors.white,
+                                titleStyle: GoogleFonts.spaceGrotesk(
+                                  fontSize: 24,
+                                  color: Colors.red,
+                                ),
+                                title: "Alert",
+                                titlePadding: EdgeInsets.only(top: 10),
+                                content: Text(
+                                  "Are you sure to go without an Image?",
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                contentPadding:
+                                    EdgeInsets.symmetric(horizontal: 12),
+                                confirm: TextButton(
                                   onPressed: () {
                                     _selectedImage =
                                         File("assets/images/name.png");
@@ -220,28 +235,41 @@ class _NameScreenState extends State<NameScreen> {
                                   child: Text(
                                     "Yes",
                                     style: TextStyle(
-                                        color: Colors.red, fontSize: 16),
-                                  )),
-                              cancel: TextButton(
+                                      color: Colors.red,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                                cancel: TextButton(
                                   onPressed: () {
                                     Get.back();
                                   },
-                                  child: Text("No",
-                                      style: TextStyle(
-                                          color: Colors.red, fontSize: 16))))
-                          : Get.off(() => const HomeScreen(),
-                              arguments: _selectedImage);
-                    }
-                  },
-                  child: Text("Get Started",
+                                  child: Text(
+                                    "No",
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            : Get.off(() => const HomeScreen(),
+                                arguments: _selectedImage);
+                      }
+                    },
+                    child: Text(
+                      "Get Started",
                       textAlign: TextAlign.center,
                       style: GoogleFonts.spaceGrotesk(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.white)),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ),
-              )
-            ]),
+              ],
+            ),
           ),
         ),
       ),
